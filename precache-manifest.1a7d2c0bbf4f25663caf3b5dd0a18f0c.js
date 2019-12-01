896b17ef85b64316c0f67bc4523a9b35402fa49d
kiten46087 <592692725@qq.com>
2019-12-01 09:59:35 -0800

    Updates

---
+++ precache-manifest.1a7d2c0bbf4f25663caf3b5dd0a18f0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eb3ec58bbc1b8d56a6d22d52b322090",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "74940d6b4a50561b6a4b",
+    "url": "/monster-rolodex/static/css/main.f5075b54.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monster-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "74940d6b4a50561b6a4b",
+    "url": "/monster-rolodex/static/js/main.b428f3ae.chunk.js"
+  },
+  {
+    "revision": "ec2849d1eae557c66f4a",
+    "url": "/monster-rolodex/static/js/runtime-main.b891af59.js"
+  }
+]);
